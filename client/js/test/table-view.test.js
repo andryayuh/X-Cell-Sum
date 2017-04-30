189f2baa2ba0a44a77d6f86b57631398d71a863c
Andrea Miralles <andreamiralles@andreamllessmbp.attlocal.net>
2017-04-30 10:22:42 -0700

    select current cell / update formula bar and current cell

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -11,8 +11,69 @@ describe('table-view', () => {
     document.documentElement.innerHTML = html;
   });
 
-    describe('table body', () => {
-      it('has the right size', () => {
+  describe('formula bar', () => {
+    it('makes changes TO the value of the current cell', () => {
+      //set up init
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect init state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[0].cells[0];
+      expect(td.textContent).toBe('');
+
+      // simulate user action
+      document.querySelector('#formula-bar').value = '65';
+      view.handleFormulaBarChange();
+
+      //inspect resulting state
+      trs = document.querySelectorAll('TBODY TR');
+      expect(trs[0].cells[0].textContent).toBe('65');
+    });
+    
+    it('updates FROM the value of the current cell', () => {
+      // set up initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({col: 2, row: 1}, '123');
+      view.init();
+
+      //inspect init state
+      const formulaBarEl = document.querySelector('#formula-bar');
+      expect(formulaBarEl.value).toBe('');
+
+      // simulate user action
+      const trs = document.querySelectorAll('TBODY TR');
+      trs[1].cells[2].click();
+
+      // inspect the resulting state
+      expect(formulaBarEl.value).toBe('123')
+    });
+  });
+
+  describe('table body', () => {
+    it('highlights the current cell when clicked', () => {
+      // set up initi state
+      const model = new TableModel(10, 5);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect init
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      // simulate user action
+      td.click();
+
+      // inspect resulting state
+      trs = document.querySelectorAll('TBODY TR')
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    });
+
+    it('has the right size', () => {
       // set up initial state
       const numCols = 6;
       const numRows = 10;
