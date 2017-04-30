659576548a9e1983eb2ac1e5dd81b1dd4ecd0a12
Andrea Miralles <andreamiralles@andreamllessmbp.attlocal.net>
2017-04-30 00:06:10 -0700

    render data rows

--- client/js/table-view.js
+++ client/js/table-view.js
@@ -13,11 +13,13 @@ class TableView {
 
   initDomReferences() {
     this.headerRowEl = document.querySelector('THEAD TR');
+    this.sheetBodyEl = document.querySelector('TBODY'); 
   }
 
 
   renderTable() {
     this.renderTableHeader();
+    this.renderTableBody();
   }
 
   renderTableHeader() {
@@ -26,6 +28,23 @@ class TableView {
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEl.appendChild(th));
   }
+
+  renderTableBody() {
+    const fragment = document.createDocumentFragment();
+    for (let row = 0; row < this.model.numRows; row++) {
+      const tr = createTR();
+      for (let col = 0; col < this.model.numCols; col++) {
+        const position = {col: col, row: row};
+        const value = this.model.getValue(position);
+        const td = createTD(value);
+        tr.appendChild(td);
+      }
+      fragment.appendChild(tr);
+    }
+    removeChildren(this.sheetBodyEl);
+    this.sheetBodyEl.appendChild(fragment);
+  }
+
 }
 
   module.exports = TableView;
