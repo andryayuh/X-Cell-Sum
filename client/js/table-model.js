20cef63b807559826edbc3e17d3ea8313816cef0
Andrea Miralles <andreamiralles@Andrea-Miralless-MacBook-Pro.local>
2017-05-03 17:09:34 -0700

    removed extra space in arrow function

--- client/js/table-model.js
+++ client/js/table-model.js
@@ -20,7 +20,7 @@ class TableModel {
   
   _getColNums(col) {
     let nums = [];
-    for (let row = 0; row < this.numRows; row++) {
+    for ( row = 0; row < this.numRows; row++) {
       const position = {col: col, row: row};
       nums.push(parseInt(this.getValue(position), 10));
     }
@@ -31,7 +31,7 @@ class TableModel {
     let colNums = this._getColNums(col)
     let sum = colNums.filter(x => !isNaN(x));
     if (sum.length > 1) {
-      sum = sum.reduce( (a, b) => a + b);
+      sum = sum.reduce((a, b) => a + b);
     }
     return sum;
   }
